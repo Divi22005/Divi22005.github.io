98d1ecef76de2e7f44c0a5f283c60179a5d05131
Divi22005 <23r11a66b8@gcet.edu.in>
2025-05-12 20:50:36 +0530

    updated

---
+++ a6/script.js
@@ -0,0 +1,7 @@
+function handleSubmit(){
+    let username = document.getElementById("user").value;
+    let password = document.getElementById("pass").value;
+    
+    console.log("Username:", username);
+    console.log("Password:", password);
+}
