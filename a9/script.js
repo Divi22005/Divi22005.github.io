75cafd01866c39d06337691f65170eb0b11348a9
Divi22005 <23r11a66b8@gcet.edu.in>
2025-05-14 21:12:46 +0530

    updated

--- a9/script.js
+++ a9/script.js
@@ -1,12 +1,26 @@
 let fruits = ["apple", "banana", "orange", "mango"];
 
-document.getElementById("searchButton").addEventListener("click", function () {
-    let input = document.getElementById("inputField").value;
-    let result = fruits.filter(fruit => fruit.toLowerCase() === input);
+const resultList = document.getElementById("result");
+const inputField = document.getElementById("inputField");
 
-    if (result.length > 0) {
-        document.getElementById("result").textContent =  " "+ result[0];
+function displayFruits(filteredFruits) {
+    resultList.innerHTML = "";
+    
+    if (filteredFruits.length > 0) {
+        filteredFruits.forEach(fruit => {
+            const li = document.createElement("li");
+            li.textContent = fruit;
+            resultList.appendChild(li);
+        });
     } else {
-        document.getElementById("result").textContent = "Item not found";
+        const li = document.createElement("li");
+        li.textContent = "Item not found";
+        resultList.appendChild(li);
     }
+}
+displayFruits(fruits);
+inputField.addEventListener("input", function () {
+    const input = this.value.toLowerCase();
+    const filtered = fruits.filter(fruit => fruit.toLowerCase().includes(input));
+    displayFruits(filtered);
 });
