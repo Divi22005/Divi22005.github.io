d550138f1709d1e93d741ea1b249d4cb436d063d
Divi22005 <23r11a66b8@gcet.edu.in>
2025-05-14 18:59:02 +0530

    updated

--- a7/script.js
+++ a7/script.js
@@ -1,9 +1,10 @@
 function calculateAll(){
     var a=Number(document.getElementById("num1").value); 
     var b=Number(document.getElementById("num2").value);
-    var sum=a+b;
-    var dif=a-b;
-    var mul=a*b;
+    var Addition=a+b;
+    var subtraction=a-b;
+    var multiplication=a*b;
+    var division=a/b;
     //var div=a/b;
    document.getElementById("result").innerHTML=
    "sum"+ sum +"<br>"+"Dif"+dif+"<br>"+"mul"+mul;
